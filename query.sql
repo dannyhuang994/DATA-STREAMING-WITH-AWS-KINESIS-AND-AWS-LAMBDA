6dc2125b360438b72ed77ef70353db5c405547e2
dannyhuang994 <60530349+dannyhuang994@users.noreply.github.com>
2020-05-25 16:04:35 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,8 @@
-SELECT Name, round(high,2) as High, ts as Time, hour as Hour
-from(
-  select stock_data.*,SUBSTRING(ts, 12, 2) as Hour, ROW_NUMBER() OVER(PARTITION BY name, SUBSTRING(ts, 12, 2) order by high) as rn
-  FROM stock_data
-)db1 where rn=1 order by name, Time
+SELECT upper(db1.Name) AS Name, round(db1.High,2) AS High, db1.Hour as Hour, ts AS Timestamp 
+FROM (
+  SELECT name AS Name, max(high) AS High, substring(ts,12,2) AS Hour
+  FROM  stock_data GROUP BY 1, 3 ORDER BY 1, 3
+  )db1, stock_data db2
+WHERE db1.Name = db2.name AND db1.Hour = substring(ts,12,2) AND db1.high = db2.High
+ORDER BY Name, Hour;
+
